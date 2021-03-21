ce226cac6e2251ebcd695eca354f16f25d983d1d
Victor I. Afolabi <javafolabi@gmail.com>
2021-03-20 20:10:23 -0500

    Add [[bin]] to Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use strtok::strtok;
+
 fn main() {
-  println!("Hello, world!");
+  let mut hello_world = "Hello world!";
+
+  println!("Before call to strtok");
+  println!("hello_world = {}", hello_world);
+
+  let hello = strtok(&mut hello_world, ' ');
+
+  println!("After call to strtok");
+  println!("hello_world = {}", hello_world);
+  println!("hello = {}", hello);
+
+  assert_eq!(hello_world, "world!");
+  assert_eq!(hello, "Hello");
 }
